7fe44f1a889ac6f996cd16cce679f7875f625113
samhitha putteti <2100031096@kluniversity.in>
2025-05-27 23:49:05 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'portfolio1', 
+  base: 'portfolio2', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
